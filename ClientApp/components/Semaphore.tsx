78e27d52b79891292da7a82883093b8aca130985
Jorge Garcia <jorge.gcia.gcia@gmail.com>
2017-09-08 13:28:44 +0200

    Add new component
    Semaphore component added

---
+++ ClientApp/components/Semaphore.tsx
@@ -0,0 +1,31 @@
+﻿import * as React from 'react';
+import { Link, RouteComponentProps } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { ApplicationState }  from '../store';
+import * as SemaphoreStore from '../store/Semaphore';
+
+type SemaphoreProps =
+    SemaphoreStore.SemaphoreState
+    & typeof SemaphoreStore.actionCreators
+    & RouteComponentProps<{}>;
+
+ class Semaphore extends React.Component<SemaphoreProps, {}> {
+    public render() {
+        return <div>
+            <h1>Semaphore</h1>
+            <p>This is a simple example of a React component.</p>
+
+            <p>Current status: <strong>{ this.props.status }</strong></p>
+
+            <button onClick={ () => { this.props.toRed() } }>To Red</button>
+            <button onClick={ () => { this.props.toYellow() } }>To Yellow</button>
+            <button onClick={ () => { this.props.toGreen() } }>To Green</button>
+        </div>;
+    }
+}
+
+// Wire up the React component to the Redux store
+export default connect(
+    (state: ApplicationState) => state.semaphore, // Selects which state properties are merged into the component's props
+    SemaphoreStore.actionCreators                 // Selects which action creators are merged into the component's props
+)(Semaphore) as typeof Semaphore;
---
+++ ClientApp/store/Semaphore.ts
@@ -0,0 +1,45 @@
+﻿import { Action, Reducer } from 'redux';
+
+// -----------------
+// STATE - This defines the type of data maintained in the Redux store.
+
+export interface SemaphoreState {
+    status: number;
+}
+
+/// -----------------
+// ACTIONS - These are serializable (hence replayable) descriptions of state transitions.
+// They do not themselves have any side-effects; they just describe something that is going to happen.
+// Use @typeName and isActionType for type detection that works even after serialization/deserialization.
+
+interface ToRedAction { type: 'TO_RED' }
+interface ToYellowAction { type: 'TO_YELLOW' }
+interface ToGreenAction { type: 'TO_GREEN' }
+
+type KnownAction = ToRedAction | ToYellowAction | ToGreenAction;
+
+export const actionCreators = {
+    toRed: () => <ToRedAction>{ type: 'TO_RED' },
+    toYellow: () => <ToYellowAction>{ type: 'TO_YELLOW' },
+    toGreen: () => <ToGreenAction>{ type: 'TO_GREEN' }
+};
+
+export const reducer: Reducer<SemaphoreState> = (state: SemaphoreState, action: KnownAction) => {
+    switch (action.type) {
+        case 'TO_RED':
+            return { status: 0 };
+        case 'TO_YELLOW':
+            return { status: 1 };
+        case 'TO_GREEN':
+            return { status: 2 };
+        default:
+            // The following line guarantees that every action in the KnownAction union has been covered by a case above
+            const exhaustiveCheck: never = action;
+    }
+
+    // For unrecognized actions (or in cases where actions have no effect), must return the existing state
+    //  (or default initial state if none was supplied)
+    return state || { status: 0 };
+};
+
+
